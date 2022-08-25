d829fb20adcf63b81f38be13eed3dacd0fa87dc0
joezerSmaniotto <joezersmaniotto5@gmail.com>
2022-08-25 07:35:41 -0300

    create component Text with styled-Componets

---
+++ src/sreens/Pets/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components/native';
+
+export const Text = styled.Text`
+  width: 95%;
+  height: 26px;
+  font-size: 16px;
+  color: blavk;
+  border: 0px solid grey;
+  border-bottom-width: 1px;
+  padding-left: 2px;
+  padding-bottom: 1px;
+  margin-bottom: 10px;
+  margin-top: 10px;
+  align-self: center;
+`;
