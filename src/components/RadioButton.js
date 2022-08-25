93b75390b03b8234cb25649fe7b24aa515c6a444
joezerSmaniotto <joezersmaniotto5@gmail.com>
2022-08-25 07:34:52 -0300

    create RadioButton

---
+++ src/components/RadioButton.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from 'styled-components/native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+// import {COLORS} from '../assets/colors';
+
+export const RadioButtonView = styled.TouchableHighlight`
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  padding-left: 20px;
+  padding-top: 10px;
+`;
+
+export const TextIcon = styled.Text`
+  font-size: 24px;
+  color: black;
+`;
+
+const RadioButton = ({label, selected, onClick}, onPress) => {
+  //console.log(label);
+  return (
+    <RadioButtonView onPress={() => onClick(label)}>
+      {selected ? (
+        <Icon name="radio-button-on-outline" size={25} color={'yellow'}>
+          <TextIcon>{label}</TextIcon>
+        </Icon>
+      ) : (
+        <Icon name="radio-button-off-outline" size={25} color={'yellow'}>
+          <TextIcon>{label}</TextIcon>
+        </Icon>
+      )}
+    </RadioButtonView>
+  );
+};
+export default RadioButton;
