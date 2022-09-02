4ffa4801e2258f86bd2b0472efba285ffebd902b
joezerSmaniotto <joezersmaniotto5@gmail.com>
2022-09-02 05:56:05 -0300

    update sreen pets, with filter

--- src/sreens/Pets/index.js
+++ src/sreens/Pets/index.js
@@ -1,13 +1,82 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useContext} from 'react';
 import {SafeAreaView, ScrollView, View, StyleSheet, Alert} from 'react-native';
-import {Input, Image, Text, Button, useTheme} from 'react-native-elements';
+import {Input, Text, Button, useTheme} from 'react-native-elements';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import CardPetList from '../../components/CardPetList';
+import ModalFilterPet from '../../components/ModalFilterPet';
 import Loading from '../../components/Loading';
+import {PetContext} from '../../context/PetProvider';
+import {UserContext} from '../../context/UserProvider';
 
 const Pets = ({navigation}) => {
   const {theme} = useTheme();
   const [loading, setLoading] = useState(false);
+  const [openModalFilterPet, setOpenModalFilterPet] = useState(false);
+  const [dataPetsFiltered, setdataPetsFiltered] = useState([]);
+  const [isFiltered, setIsFiltered] = useState(false);
+  const [parametrosFiltrosPets, setParametrosFiltrosPets] = useState({
+    raca: '',
+    tipo: 1,
+    sexo: 2,
+  });
+  const {getPetsView, petsListView} = useContext(PetContext);
+  const {getUser, userE} = useContext(UserContext);
+
+  useEffect(() => {
+    //  set isMounted to true
+    let isMounted = true;
+    // 👇️ only update state if component is mounted
+    if (isMounted) {
+      getPetsView({
+        raca: '',
+        tipo: 0,
+        sexo: 2,
+      });
+    }
+    return () => {
+      //  when component unmounts, set isMounted to false
+      isMounted = false;
+      // setdataPetsFiltered(false);
+      // setParametrosFiltrosPets({raca: '', tipo: 1, sexo: 2});
+    };
+    // eslint-disable-next-line
+  }, []);
+  useEffect(() => {
+    getUser();
+    // eslint-disable-next-line
+  }, []);
+
+  useEffect(() => {
+    setdataPetsFiltered(petsListView);
+  }, [petsListView]);
+
+  useEffect(() => {
+    // Tipo
+    let data = petsListView.filter(pet => {
+      return pet.tipo === parametrosFiltrosPets.tipo;
+    });
+
+    // Sexo
+    if (parametrosFiltrosPets.sexo !== 2) {
+      data = data.filter(pet => {
+        return pet.sexo === parametrosFiltrosPets.sexo;
+      });
+    }
+
+    // Raca
+    if (parametrosFiltrosPets.raca !== '') {
+      data = data.filter(pet => {
+        return pet.raca === parametrosFiltrosPets.raca;
+      });
+    }
+    if (parametrosFiltrosPets.isFiltered) {
+      setIsFiltered(true);
+    }
+
+    console.log('dataFiltroAplicado: ', data);
+    setdataPetsFiltered(data);
+    //"raca": "Labrador retriver", "sexo": 0, "tipo": 0,
+  }, [parametrosFiltrosPets, petsListView]);
 
   return (
     <SafeAreaView style={styles.container}>
@@ -22,7 +91,7 @@ const Pets = ({navigation}) => {
               color={theme.colors.primary}
               size={35}
               onPress={() => {
-                Alert.alert('Oi', 'Teste');
+                setOpenModalFilterPet(!openModalFilterPet);
               }}
             />
           }
@@ -31,16 +100,49 @@ const Pets = ({navigation}) => {
           containerStyle={styles.buttoncontainerStyle}
         />
       </View>
-      <ScrollView>
+      <ScrollView style={{flex: 1}}>
         <View style={styles.divInferior}>
-          <CardPetList />
+          {!isFiltered ? (
+            petsListView.map(item => {
+              return <CardPetList key={item.uid} dados={item} user={userE} />;
+            })
+          ) : dataPetsFiltered.length > 0 ? (
+            dataPetsFiltered.map(item => {
+              return <CardPetList key={item.uid} dados={item} user={userE} />;
+            })
+          ) : (
+            <View style={styles.emptyContent}>
+              <MaterialIcons
+                name="pets"
+                color={theme.colors.primary}
+                size={60}
+                onPress={() => {
+                  setOpenModalFilterPet(!openModalFilterPet);
+                }}
+              />
+              <Text
+                style={{
+                  color: theme.colors.black,
+                  fontSize: 19,
+                  marginTop: 10,
+                }}>
+                Não existe Pets para os filtros aplicados!
+              </Text>
+            </View>
+          )}
         </View>
       </ScrollView>
       {loading && <Loading />}
+
+      <ModalFilterPet
+        visible={openModalFilterPet}
+        setVisible={setOpenModalFilterPet}
+        setParametrosFiltrosPets={setParametrosFiltrosPets}
+      />
     </SafeAreaView>
   );
 };
-export default Pets;
+export default React.memo(Pets);
 
 const styles = StyleSheet.create({
   container: {
@@ -118,5 +220,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#e5e5e5',
     alignSelf: 'flex-end',
     padding: 10,
+  },
+  emptyContent: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 80,
   },
 });
