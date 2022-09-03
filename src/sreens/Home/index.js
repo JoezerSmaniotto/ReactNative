db9c1566d6e0c744763f044727541a9f6158d6f1
joezerSmaniotto <joezersmaniotto5@gmail.com>
2022-09-03 20:38:53 -0300

    upgrade Navigate icons from Home

--- src/sreens/Home/index.js
+++ src/sreens/Home/index.js
@@ -1,32 +1,56 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import Ionicons from 'react-native-vector-icons/Ionicons';
 import {useTheme} from 'react-native-elements';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import MyPets from '../MyPets';
 import Pets from '../Pets';
 import User from '../User';
 import PetsMap from '../PetsMap';
+import {PetContext} from '../../context/PetProvider';
 
 const Tab = createBottomTabNavigator();
 const Home = ({navigation}) => {
+  const {petsList} = useContext(PetContext);
   const {theme} = useTheme();
   return (
     <Tab.Navigator
-      screenOptions={{
+      screenOptions={({route}) => ({
         headerShown: false,
         tabBarActiveTintColor: theme.colors.primary,
-        tabBarInactiveTintColor: theme.colors.blacks,
-      }}>
+        tabBarInactiveTintColor: theme.colors.black,
+        tabBarStyle: {height: 60},
+        tabBarLabelStyle: {
+          fontSize: 11,
+          fontWeight: 'bold',
+          marginTop: -5,
+          marginBottom: 5,
+        },
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName;
+
+          if (route.name === 'Pets') {
+            iconName = 'pets';
+          } else if (route.name === 'MyPets') {
+            iconName = 'pets';
+          } else if (route.name === 'User') {
+            iconName = 'people';
+          } else if (route.name === 'PetsMap') {
+            iconName = 'map';
+          }
+          return (
+            <MaterialIcons
+              name={iconName}
+              size={focused ? size + 14 : size}
+              color={color}
+            />
+          );
+        },
+      })}>
       <Tab.Screen
         name="Pets"
         component={Pets}
         options={{
           tabBarLabel: 'Pets',
-          tabBarIcon: () => (
-            <MaterialIcons name="pets" color={theme.colors.primary} size={20} />
-          ),
         }}
       />
       <Tab.Screen
@@ -34,19 +58,6 @@ const Home = ({navigation}) => {
         component={MyPets}
         options={{
           tabBarLabel: 'MyPets',
-          tabBarIcon: () => (
-            <MaterialIcons name="pets" color={theme.colors.primary} size={20} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="User"
-        component={User}
-        options={{
-          tabBarLabel: 'User',
-          tabBarIcon: () => (
-            <Ionicons name="people" color={theme.colors.primary} size={20} />
-          ),
         }}
       />
       <Tab.Screen
@@ -54,13 +65,14 @@ const Home = ({navigation}) => {
         component={PetsMap}
         options={{
           tabBarLabel: 'PetsMap',
-          tabBarIcon: () => (
-            <MaterialCommunityIcons
-              name="map-marker-circle"
-              color={theme.colors.primary}
-              size={25}
-            />
-          ),
+          tabBarBadge: petsList.length, // exemplo de badge só tens que colocar em state
+        }}
+      />
+      <Tab.Screen
+        name="User"
+        component={User}
+        options={{
+          tabBarLabel: 'User',
         }}
       />
     </Tab.Navigator>
